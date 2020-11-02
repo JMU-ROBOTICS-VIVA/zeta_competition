a5265f88e35027289748804225f6038fa8581c84
Nathan Sprague <spragunr@jmu.edu>
2020-11-01 19:32:25 -0500

    initial commit

---
+++ zeta_competition/launch/competition.launch.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+#
+# Copyright 2019 ROBOTIS CO., LTD.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Authors: Darby Lim
+
+import os
+
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch_ros.actions import Node
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import ThisLaunchFileDir
+from launch.actions import ExecuteProcess
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+from launch.actions import SetEnvironmentVariable
+from launch import LaunchContext
+
+TURTLEBOT3_MODEL = os.environ['TURTLEBOT3_MODEL']
+
+def generate_launch_description():
+
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='True')
+    this_path = get_package_share_directory('zeta_competition')
+
+    tb_launch_file_dir = os.path.join(get_package_share_directory('turtlebot3_gazebo'), 'launch')
+    nav2_launch_file_dir = os.path.join(get_package_share_directory('jmu_turtlebot3_bringup'), 'launch')
+
+    default_world_path = os.path.join(this_path, 'worlds', 'room_practice.world')
+    default_map_path = os.path.join(this_path, 'maps', 'room_practice.yaml')
+
+    model_path = os.path.join(this_path, 'models/')
+    tb_model_path = os.path.join(get_package_share_directory('turtlebot3_gazebo'), 'models/')
+
+    lc = LaunchContext()
+    SetEnvironmentVariable('GAZEBO_RESOURCE_PATH',
+                           '/usr/share/gazebo-9:' + model_path + ":" + tb_model_path).visit(lc)
+    SetEnvironmentVariable('GAZEBO_MODEL_PATH',
+                           '/usr/share/gazebo-9/models:'+ model_path + ":" + tb_model_path).visit(lc)
+
+    return LaunchDescription([
+        DeclareLaunchArgument('world', default_value=default_world_path),
+        DeclareLaunchArgument('map', default_value=default_map_path),
+
+        # START SIMULATOR
+        ExecuteProcess(
+            cmd=['gazebo', '--verbose', LaunchConfiguration('world'), '-s', 'libgazebo_ros_init.so'],
+            output='screen'),
+
+        ExecuteProcess(
+            cmd=['ros2', 'param', 'set', '/gazebo', 'use_sim_time', use_sim_time],
+            output='screen'),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([tb_launch_file_dir, '/robot_state_publisher.launch.py']),
+            launch_arguments=[('use_sim_time', use_sim_time)],
+        ),
+
+        # START NAV SYSTEM
+        
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([nav2_launch_file_dir, '/navigation2.launch.py']),
+            launch_arguments=[
+                ('map', LaunchConfiguration('map')),
+                ('use_sim_time', use_sim_time)
+            ],
+        ),
+        
+    ])
+
+if __name__ == "__main__":
+    generate_launch_description()
---
+++ zeta_competition/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+from glob import glob
+import os
+
+package_name = 'zeta_competition'
+
+model_data_files = []
+for cur_dir, _, files in os.walk('models'):
+    to = 'share/' + package_name + '/' + cur_dir + "/"
+    model_data_files.append((to, [cur_dir + "/" + f for f in files]))
+
+data_files=[
+    ('share/ament_index/resource_index/packages',
+     ['resource/' + package_name]),
+    ('share/' + package_name, ['package.xml']),
+    ('share/' + package_name + '/launch/', glob('launch/*.launch.py')),
+    ('share/' + package_name + '/worlds/', glob('worlds/*.world')),
+    ('share/' + package_name + '/maps/', glob('maps/*.yaml')),
+    ('share/' + package_name + '/maps/', glob('maps/*.pgm'))] + model_data_files
+#print(data_files)
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=data_files,
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='spragunr',
+    maintainer_email='nathan.r.sprague@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
