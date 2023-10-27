83073077d525f75c62190b3ec766e800287a799f
Nathan Sprague <nathan.r.sprague@gmail.com>
2023-10-27 16:35:45 -0400

    consolidate competition and scoring launch files.

--- zeta_competition/launch/competition.launch.py
+++ zeta_competition/launch/competition.launch.py
@@ -17,7 +17,6 @@
 # Authors: Darby Lim
 
 import os
-import time
 
 import rclpy
 from ament_index_python.packages import get_package_share_directory
@@ -25,10 +24,8 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch_ros.actions import Node
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration, PythonExpression
+from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument
-from launch.actions import SetEnvironmentVariable
-from launch import LaunchContext
 from launch.conditions import IfCondition
 from launch.conditions import UnlessCondition
 
@@ -47,16 +44,20 @@ def get_node_names():
 def generate_launch_description():
 
     use_sim_time = LaunchConfiguration('use_sim_time', default='True')
+    scorer = LaunchConfiguration('scoring', default='False')
     this_path = get_package_share_directory('zeta_competition')
 
     nav2_launch_file_dir = os.path.join(get_package_share_directory('turtlebot3_navigation2'), 'launch')
 
     default_map_path = os.path.join(this_path, 'maps', 'room_practice.yaml')
     default_pose = os.path.join(this_path, 'config', 'sim_initial_pose.yaml')
+    rviz_config_path = os.path.join(this_path, 'rviz', 'final_scoring.rviz')
 
     ld = LaunchDescription([
         DeclareLaunchArgument('map', default_value=default_map_path),
         DeclareLaunchArgument('initial_pose', default_value=default_pose),
+        DeclareLaunchArgument('scoring', default_value='false'),
+        DeclareLaunchArgument('ground_truth', default_value=os.path.join(this_path, 'config/default_victims.csv')),
 
         DeclareLaunchArgument(
             'use_sim_time',
@@ -80,20 +81,40 @@ def generate_launch_description():
                 ('use_sim_time', LaunchConfiguration('use_sim_time'))
             ],
         ),
-        
-        # Start the reporting button
-        Node(
-            package="zeta_competition",
-            executable="report_button",
-            output="screen",
-        ),
-        
+
         # Start initial pose setter
         Node(
             package="zeta_competition",
             executable="set_initial_pose",
             output="screen",
             parameters=[LaunchConfiguration('initial_pose')]
+        ),
+
+        # Start the reporting button
+        Node(
+            package="zeta_competition",
+            executable="report_button",
+            output="screen",
+            condition=UnlessCondition(LaunchConfiguration("scoring"))
+        ),
+
+        Node(
+            package="zeta_competition",
+            executable="zeta_scorer",
+            name="zeta_scorer",
+            output="screen",
+            arguments=[LaunchConfiguration('ground_truth')],
+            condition=IfCondition(LaunchConfiguration("scoring"))
+        ),
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2_scoring',
+            arguments=['-d', rviz_config_path],
+            parameters=[{'use_sim_time': use_sim_time}],
+            output={'both': 'log'},
+            condition=IfCondition(LaunchConfiguration("scoring"))
         ),
 
     ])
